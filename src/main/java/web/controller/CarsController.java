86de422b1394cd30e75308845bfccea55f2fc700
Mxm06 <stingermax06@mail.ru>
2022-12-20 13:18:16 +0500

    Task completed 1.1

--- src/main/java/web/controller/CarsController.java
+++ src/main/java/web/controller/CarsController.java
@@ -1,5 +1,6 @@
 package web.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,9 +12,13 @@ import web.service.CarServiceImpl;
 
 @Controller
 public class CarsController {
+    CarService carService;
+    @Autowired
+    public CarsController(CarService carService) {
+        this.carService = carService;
+    }
     @GetMapping(value = "cars")
     public String printCars(@RequestParam(name = "count",defaultValue = "5")int count, Model model) {
-        CarService carService = new CarServiceImpl();
         model.addAttribute("carList",carService.showCars(Car.getCarList(),count));
         return "cars";
     }
--- src/main/java/web/service/CarServiceImpl.java
+++ src/main/java/web/service/CarServiceImpl.java
@@ -1,13 +1,14 @@
 package web.service;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
 import web.Model.Car;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
-
+@Component
 public class CarServiceImpl implements CarService{
-
     @Override
     public List<Car> showCars(List<Car> carList, int count) {
         return carList.stream().limit(count).collect(Collectors.toList());
